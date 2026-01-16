8ccb2178262615cd95dd67d7c40b47810c819109
Valeri Hristov <valericfbg@gmail.com>
2026-01-16 12:14:44 +0200

    drafting an idea about the textures.

--- platform/resources/CPUTextureHandle.h
+++ platform/resources/CPUTextureHandle.h
@@ -5,7 +5,10 @@
 
 namespace Platform::Resources {
 
-class TextureHandle {
+// Idea: So the Texture manager has to async load textures from disk to memory and then on update() it should 
+// pump the textures to GPU
+// Or better yet, another class which will take care of the storage of the textures will upload them to GPU.
+class CPUTextureHandle {
 	ITexture* ptr = nullptr; ///< Pointer to the resource. Read-only, do not delete.
 	bool isReady = false; ///< Is the resource loaded.
 	// TODO: There is a concept of 'generations' but i am still not familiar with it.
