c1e060492875d7bb9b8c2513328ea41af588b2c1
Valeri Hristov <valericfbg@gmail.com>
2026-03-21 18:46:16 +0200

    Writing a cool idea about parallel IO and serial decoding.

--- backend/sdl/resources/SDLCPUTextureResourceLoader.cpp
+++ backend/sdl/resources/SDLCPUTextureResourceLoader.cpp
@@ -13,6 +13,20 @@ IdOrError SDLCPUTextureResourceLoader::load(const std::filesystem::path& resourc
 {
 	assert(!resourceFile.empty());
 
+	// TODO: hmmmmmm okay so we can actually have a parallel I/O loader
+	// which will load the files in parallel and then will call IMG_Load_RW
+	// in serial because it seems the IMG decoding is not thread safe in SDL...
+	// So this method will be kinda redundant...anyway leave it for now and add a new method
+	// that will accept a list of resource files.
+	// But idk if the parallel file loader should be implemented here...
+	// Maybe another class that can load files in parallel and provide pointers
+	// to the data is better.
+	// Maybe the freelist can be useful here too.
+	// So this method here will accept a...vector of pointers?
+	// or maybe it will have a reference to the parallel file loading class and fetch
+	// the ids from there? No thats kinda awkward....
+	// So at the end it turns out that both the GPU and the CPU decoding can't be 
+	// done in parallel hahahhaha so....its kinda funny but anyway....
 	SDL_Surface* loadedSurface = IMG_Load((const char*)(resourceFile.u8string().c_str()));
 
 	if (!loadedSurface) {
