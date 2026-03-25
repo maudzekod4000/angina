0f6f27092ff27fef13a7a060e298220f2fe54ac9
Valeri Hristov <valericfbg@gmail.com>
2026-03-25 17:10:44 +0200

    Added some tests for the resource loading and for a lot of textures the async variant is super fast

--- tests/resources/test_SDLCPUTextureResourceLoaderAsync.cpp
+++ tests/resources/test_SDLCPUTextureResourceLoaderAsync.cpp
@@ -1,12 +1,18 @@
 #include <gtest/gtest.h>
 
 #include <string>
-#include <filesystem>
+#include <chrono>
 #include <vector>
+#include <string>
+#include <filesystem>
+#include <iostream>
 
-#include "backend/sdl/resources/SDLCPUTextureResourceLoader.h"
+#include "backend/sdl/resources/SDLCPUTextureResourceLoaderAsync.h"
 
 using namespace Backend::SDL::Resources;
+
+// Note: Always use isValid before calling release or resolve!
+// Tests are like usage examples.
 
 static std::vector<std::filesystem::path> loadFileNames(const std::filesystem::path& dir)
 {
@@ -25,69 +31,21 @@ static std::vector<std::filesystem::path> loadFileNames(const std::filesystem::p
     return res;
 }
 
-// Note: Always use isValid before calling release or resolve!
-// Tests are like usage examples.
-
-TEST(SDLCPUTextureResource, Init)
+TEST(SDLCPUTextureResourceAsync, Init)
 {
-    SDLCPUTextureResourceLoader texLoader;
+    SDLCPUTextureLoaderAsync texLoader;
 
     EXPECT_TRUE(&texLoader);
 }
 
-// 1. Test that we can load a resource and get a handle to it.
-TEST(SDLCPUTextureResource, Load)
-{
-    SDLCPUTextureResourceLoader texLoader;
-
-    const std::string path = std::string(TEST_RESOURCE_DIR) + "/Bishop_W.png";
-
-    const auto idOrErr = texLoader.load(path);
-
-    EXPECT_TRUE(idOrErr.has_value());
-    EXPECT_TRUE(texLoader.isValid(idOrErr.value()));
-    auto handle = texLoader.resolve(idOrErr.value());
-    EXPECT_TRUE(handle.ptr);
-}
-
-// 2. Test that we can remove a resource and it is no longer valid (how to check the memory tho)
-TEST(SDLCPUTextureResource, Release)
-{
-    SDLCPUTextureResourceLoader texLoader;
-
-    const std::string path = std::string(TEST_RESOURCE_DIR) + "/Bishop_W.png";
-
-    const auto idOrErr = texLoader.load(path);
-    const auto id = idOrErr.value();
-
-    EXPECT_TRUE(texLoader.isValid(id));
-    
-    auto err = texLoader.release(id);
-
-    EXPECT_FALSE(err);
-    EXPECT_FALSE(texLoader.isValid(id));
-}
-
-// 3. Loading an non-existent resource should return a good error
-TEST(SDLCPUTextureResource, LoadNonExist)
-{
-    SDLCPUTextureResourceLoader texLoader;
-
-    const std::string path = std::string(TEST_RESOURCE_DIR) + "/not-really-there.png";
-
-    const auto idOrErr = texLoader.load(path);
-
-    EXPECT_FALSE(idOrErr.has_value());
-}
-
-TEST(SDLCPUTextureResource, Load10)
+TEST(SDLCPUTextureResourceAsync, Load10)
 {
     const std::string dir = std::string(TEST_RESOURCE_DIR) + "/textures10";
     const std::vector<std::filesystem::path> texFilePaths = loadFileNames(dir);
 
     EXPECT_EQ(10, texFilePaths.size());
 
-    SDLCPUTextureResourceLoader texLoader;
+    SDLCPUTextureLoaderAsync texLoader;
 
     auto start = std::chrono::steady_clock::now();
     auto idsOrErrors = texLoader.load(texFilePaths);
@@ -103,7 +61,7 @@ TEST(SDLCPUTextureResource, Load10)
     }
 }
 
-TEST(SDLCPUTextureResource, Load50)
+TEST(SDLCPUTextureResourceAsync, Load50)
 {
     const std::string dir = std::string(TEST_RESOURCE_DIR) + "/textures10";
     const std::vector<std::filesystem::path> texFilePaths = loadFileNames(dir);
@@ -119,7 +77,7 @@ TEST(SDLCPUTextureResource, Load50)
 
     EXPECT_EQ(50, texFilePaths50.size());
 
-    SDLCPUTextureResourceLoader texLoader;
+    SDLCPUTextureLoaderAsync texLoader;
 
     auto start = std::chrono::steady_clock::now();
     auto idsOrErrors = texLoader.load(texFilePaths50);
