2b51eb73b36e1476dfd0e36a66a1876926c02435
Valeri Hristov <valericfbg@gmail.com>
2026-08-19 15:46:02 +0300

    Replacing dynamic_cast with assert + static_cast as it would be a programming error if the cast fails

--- backend/sdl/rendering/SDLRenderer.cpp
+++ backend/sdl/rendering/SDLRenderer.cpp
@@ -22,29 +22,22 @@ std::shared_ptr<SDLRenderer> SDLRenderer::makeShared(SDL_Renderer* r)
 
 void SDLRenderer::render(Platform::Resources::TextureHandle tex)
 {
-	// TODO: Let's think how we can avoid dynamic_casts.
-	// I think that templates will help us.
-	SDLGPUTexture* sdlTexWrapper = dynamic_cast<SDLGPUTexture*>(tex.ptr);
+	assert(dynamic_cast<SDLGPUTexture*>(tex.ptr));
 
-	assert(sdlTexWrapper && "render: expected SDLGPUTexture");
+	SDLGPUTexture* sdlTexWrapper = static_cast<SDLGPUTexture*>(tex.ptr);
 
-	auto sdlTex = sdlTexWrapper->get();
+	SDL_Texture* sdlTex = sdlTexWrapper->get();
 
-	// There should be another part of the system which makes sure all the pointers
-	// are valid when they reach this point...
+	assert(sdlTex);
 	const int res = SDL_RenderCopy(renderer, sdlTex, NULL, NULL);
-
-	// Even if the result is non-zero....what ? we can't keep printing errors all the time.
-	// It will be a mess...But still it's not a bad idea to have some logging
-	// I think it's useful to have a logger which can deduplicate errors
-	// This scenario happened on the Arena project too and it would be useful even now.
 
 	assert(res == 0);
 }
 
 void SDLRenderer::clear()
 {
-	SDL_RenderClear(renderer);
+	const int res = SDL_RenderClear(renderer);
+	assert(res == 0);
 }
 
 void SDLRenderer::present()
@@ -52,29 +45,16 @@ void SDLRenderer::present()
 	SDL_RenderPresent(renderer);
 }
 
-// So there should be a class that is a texture loader.
-// it should have a reference to the CPU texture loader and
-// a reference to the converter...
-// Now that's a bit shitty because that means that we should always convert,....
-// but it could be made in a way that does not require this or maybe the 
-// converter will be a noop? hmmmm
-
-// Lets think of the usual flow of things:
-// 1. We load the textures into main memory in parallel (whatever)
-// 2. We need a way to transfer them to GPU and delete the CPU resources...
-// 3. We need a way to reference the GPU resources by Id.....
-// It would be good to reuse the Id from the CPU loading.
-// So that could be a class that does all those things.
-
 std::expected<TextureHandle, ErrorCode> SDLRenderer::transferGPU(TextureHandle cpuTex)
 {
-	// I think with the use of some templates we can avoid using dynamic_cast.
-	SDLCPUTexture* sdlTexWrapper = dynamic_cast<SDLCPUTexture*>(cpuTex.ptr);
-	if (!sdlTexWrapper) {
-		return std::unexpected(ErrorCode(-1, "cast failed: expected handle to SDLCPUTexture", ANGINA_CURRENT_FUNCTION));
-	}
+	assert(dynamic_cast<SDLCPUTexture*>(cpuTex.ptr));
+	SDLCPUTexture* sdlTexWrapper = static_cast<SDLCPUTexture*>(cpuTex.ptr);
 
-	SDL_Texture* sdlTex = SDL_CreateTextureFromSurface(renderer, sdlTexWrapper->get());
+	SDL_Surface* sdlSurface = sdlTexWrapper->get();
+
+	assert(sdlSurface);
+
+	SDL_Texture* sdlTex = SDL_CreateTextureFromSurface(renderer, sdlSurface);
 
 	if (!sdlTex) {
 		return std::unexpected(Backend::SDL::Error::make(ANGINA_CURRENT_FUNCTION));
