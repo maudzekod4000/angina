e158c48c57aab6addfe141d9d561409f31f13a54
Valeri Hristov <valericfbg@gmail.com>
2026-09-01 12:59:02 +0300

    outlining the plan to simplify the engine

---
+++ backend/sdl/resources/SDLTexture.h
@@ -0,0 +1,13 @@
+/// ok sooo this will be a simple wrapper around SDL.
+/// namespaces are OK...getters ....idk....i think they are fine
+/// but...i trust myself and i think i wont set a value after construct...
+/// 
+
+/// Namespaces are better than prefixes haha
+namespace Backend::SDL::Resources {
+	struct SDLTexture {
+
+		int width = 0;
+		int height = 0;
+	};
+}
--- platform/resources/Texture.h
+++ platform/resources/Texture.h
@@ -3,6 +3,11 @@
 
 namespace Platform::Resources {
 
+/// Hmm, maybe i want to go with a simpler C-inspired design for this engine.
+/// I feel the OOP things getting complex and we won't have two implementations
+/// ever because this will be a concrete game.
+/// so there will be a thin wrapper around the SDL_Texture that will 
+/// hold the pointer, be able to delete things and also cache the width and height.
 struct Texture {
 	virtual ~Texture() {}
 
