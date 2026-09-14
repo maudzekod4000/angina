02c1cdd1b3b87b20bde9a92cbdf4fa77f1ee7628
Valeri Hristov <valericfbg@gmail.com>
2026-09-14 15:31:47 +0300

    Simplifying the SpriteAnim class a bit.

--- engine/GameObject.h
+++ engine/GameObject.h
@@ -12,11 +12,6 @@ namespace Angina::EngineV3 {
 	struct SpriteAnim {
 
 		SpriteAnim(int frameCount): frameCount(frameCount) {}
-
-		// We need:
-		// 1. a way to start the animation
-		// 2. a way to reset the animation
-		// 3. a way to stop the animation
 		
 		// TODO: THink: Hmmm i think this kinda time interpolation
 		// and the kind that will be used for movement might share similar properties
@@ -27,41 +22,24 @@ namespace Angina::EngineV3 {
 		void start(int animDurMs) {
 			animDurationMs = animDurMs;
 			animationStopwatch.reset();
-			active = true;
 		}
 
 		void stop() {
-			active = false;
+			animDurationMs = 0;
 		}
 
 		void update() {
-			assert(animDurationMs > 0);
-			if (!active || frameCount == 1) return; // This is a single frame sprite, i.e. a normal texture.
+			if (animDurationMs == 0 || frameCount == 1) return;
 
-			// TODO: Do some calculations to figure out the current frame based on the time that passed
-			// between starting the animation and now.
 			long long timeSinceStartMs = std::chrono::duration_cast<std::chrono::milliseconds>(animationStopwatch.elapsed()).count();
 
 			float animProgressPercent = timeSinceStartMs / float(animDurationMs);
 			float animPercentNormalized = animProgressPercent - int(animProgressPercent);
 			currentFrameIdx = int(frameCount * animPercentNormalized);
-
-			// TODO: I think this calculation can be optimized.
 		}
-
-		// Design:
-		// we need to track when the animation started and how long it has been running
-		// in order to figure out which frame to show in the case of multiple frames.
-		// if there is just a single frame then we just show that frame.
-
-		// We can create another constructor that will just set a single frame and will not deal
-		// with any time management.
-
-		// another important thing is that we need to be able to stop the animation.
 
 		int currentFrameIdx = 0;
 	private:
-		bool active = false;
 		int frameCount = 0;
 		int animDurationMs = 0;
 		Core::Time::Stopwatch animationStopwatch;
